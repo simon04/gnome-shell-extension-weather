95752e5f015bbe37674399cc0a064a19a656e67f
ecyrbe <ecyrbe@gmail.com>
2011-05-26 06:57:50 +0200

    Update gettext domain
    the domain wasn't correct for the project name

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gnome-shell-extensions"
+PKG_NAME="gnome-shell-extension-weather"
 
 test -f $srcdir/configure.ac || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
