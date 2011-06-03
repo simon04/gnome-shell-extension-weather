0fe8ed2735d67b1b4ac4dd05fe130798d98d1516
simon04 <Simon.Legner@gmail.com>
2011-06-03 22:03:51 +0200

    Source code formatting (comment).

--- src/extension.js
+++ src/extension.js
@@ -1,31 +1,32 @@
 /*
-
- *  Weather extension for Gnome shell
- *  - Displays a small weather information on the top panel
- *  - On click, gives a popup with details about the weather
-
-    Copyright (C) 2011
-        ecyrbe <ecyrbe+spam@gmail.com>,
-        Timur Kristof <venemo@msn.com>,
-        Elad Alfassa <elad@fedoraproject.org>,
-        Simon Legner <Simon.Legner@gmail.com>
-
-    This file is part of gnome-shell-extension-weather.
-
-    gnome-shell-extension-weather is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    gnome-shell-extension-weather is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with gnome-shell-extension-weather.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+ *
+ *  Weather extension for GNOME Shell
+ *  - Displays a small weather information on the top panel.
+ *  - On click, gives a popup with details about the weather.
+ *
+ * Copyright (C) 2011
+ *     ecyrbe <ecyrbe+spam@gmail.com>,
+ *     Timur Kristof <venemo@msn.com>,
+ *     Elad Alfassa <elad@fedoraproject.org>,
+ *     Simon Legner <Simon.Legner@gmail.com>
+ *
+ *
+ * This file is part of gnome-shell-extension-weather.
+ *
+ * gnome-shell-extension-weather is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gnome-shell-extension-weather is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gnome-shell-extension-weather.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
