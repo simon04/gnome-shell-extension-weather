12c8723c41750280696f90ad465aa06fdd48e523
simon04 <Simon.Legner@gmail.com>
2012-03-29 22:50:09 +0200

    fix #57 - update all po files - some translation required

--- po/update.sh
+++ po/update.sh
@@ -4,6 +4,7 @@ POT=gnome-shell-extension-weather.pot
 TMP=messages.po
 TMP2=messages2.po
 
+# update pot
 echo '' > $TMP
 pushd ../src
 xgettext -o ../po/$TMP -L python -j --keyword=_ extension.js
@@ -11,3 +12,6 @@ popd
 msgmerge -N $POT $TMP > $TMP2
 mv $TMP2 $POT
 rm -f $TMP $TMP2
+
+# update po's
+for po in *po; do msgmerge -U $po gnome-shell-extension-weather.pot; done
